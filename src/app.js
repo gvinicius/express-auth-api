7f3a04fce4fb9d9f077fc73bbde5eb243780d70f
Vinicius Lins Gesteira <vlinsufba@gmail.com>
2026-07-26 19:23:36 -0300

    feat(api): add /quotes, /quotes/random, /authors, and /health endpoints

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const compression = require('compression');
 
 const auth = require('./controllers/authController');
 const login = require('./controllers/loginController');
+const poetry = require('./controllers/poetryController');
 
 app.use(compression());
 app.use(bodyParser.json());
@@ -14,5 +15,11 @@ app.use(bodyParser.json());
 app.post('/auth', async (req, res, next) => auth.proctectRoute(req, res, next));
 app.post('/signup', async (req, res, next) => login.signup(req, res, next));
 app.post('/signin', async (req, res, next) => login.signin(req, res, next));
+
+// Poetry routes
+app.get('/health', poetry.health);
+app.get('/quotes', poetry.getQuotes);
+app.get('/quotes/random', poetry.getRandom);
+app.get('/authors', poetry.searchAuthors);
 
 module.exports = app;
---
+++ src/controllers/poetryController.js
@@ -0,0 +1,51 @@
+const { aggregate } = require('../services/poetry');
+
+const poetry = {};
+
+poetry.health = async (req, res) => {
+  res.status(200).json({ status: 'ok' });
+};
+
+poetry.getQuotes = async (req, res) => {
+  const { author, lang, genre, q, source, limit } = req.query;
+  try {
+    const results = await aggregate({ author, lang, genre, q, source, limit });
+    res.status(200).json({ count: results.length, results });
+  }
+  catch (e) {
+    res.status(200).json({ count: 0, results: [] });
+  }
+};
+
+poetry.getRandom = async (req, res) => {
+  const { source, limit } = req.query;
+  try {
+    const results = await aggregate({ source, limit });
+    // Shuffle naive
+    for (let i = results.length - 1; i > 0; i -= 1) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const tmp = results[i];
+      results[i] = results[j]; // eslint-disable-line no-param-reassign
+      results[j] = tmp; // eslint-disable-line no-param-reassign
+    }
+    res.status(200).json({ count: results.length, results });
+  }
+  catch (e) {
+    res.status(200).json({ count: 0, results: [] });
+  }
+};
+
+poetry.searchAuthors = async (req, res) => {
+  const { author, source } = req.query;
+  try {
+    const results = await aggregate({ author, source, limit: 20 });
+    const authors = Array.from(new Set(results.map((r) => r.author).filter(Boolean)));
+    res.status(200).json({ count: authors.length, results: authors });
+  }
+  catch (e) {
+    res.status(200).json({ count: 0, results: [] });
+  }
+};
+
+module.exports = poetry;
+
